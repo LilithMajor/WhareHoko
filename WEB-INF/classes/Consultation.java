95f0d8f771bcc7faf2c776eeea970cecd91de83c
Alex BOURNADET <bournade@iut.univ-paris5.fr>
2017-05-19 12:20:42 +0200

    Mise en vente

--- WEB-INF/classes/Consultation.java
+++ WEB-INF/classes/Consultation.java
@@ -25,7 +25,7 @@ public static final String ATT_SESSION_USER = "sessionUtilisateur";
         /* Préparation de l'objet formulaire */
     	Database db = Database.getDatabase();
         HttpSession session = request.getSession(true);
-        ArrayList<Appartement> apparts = new ArrayList();
+        ArrayList<Appartement> apparts = new ArrayList<Appartement>();
         
         /* Traitement de la requête et récupération du bean en résultant */
 		try {
@@ -35,47 +35,11 @@ public static final String ATT_SESSION_USER = "sessionUtilisateur";
 		} catch (SQLException e) {
 			System.out.println("SQLException");
 		}
-		request.setAttribute("apparts", apparts);
-	    this.getServletContext().getRequestDispatcher("/consulterAppart.jsp").forward( request, response );
+		request.setAttribute("appartsAmoi", apparts);
+	    this.getServletContext().getRequestDispatcher("/WEB-INF/consulterAppart.jsp").forward( request, response );
     }
 
     public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-        
-        /* Préparation de l'objet formulaire */
-        RequetesBDD form = new RequetesBDD();
-        HttpSession session = request.getSession(true);
-        Boolean erreur = false;
-        ArrayList<Appartement> apparts = new ArrayList();
-        String parent = "connexion";
-        
-        /* Traitement de la requête et récupération du bean en résultant */
-        Proprietaire prop = new Proprietaire();
-		try {
-			prop = form.connecterUtilisateur( request );
-			apparts = form.getAllAppartements(request);
-		} catch (ClassNotFoundException e) {
-			System.out.println("ClassNotFound");
-		} catch (SQLException e) {
-			System.out.println("SQLException");
-		} catch (NullPointerException e){
-			erreur = true;
-			request.setAttribute("erreur", erreur);
-			this.getServletContext().getRequestDispatcher("/WEB-INF/connexion.jsp").forward( request, response );
-		}
-
-        /* Récupération de la session depuis la requête */
-        
-
-        /**
-         * Si aucune erreur de validation n'a eu lieu, alors ajout du bean
-         * Utilisateur à la session, sinon suppression du bean de la session.
-         */
-		if(!erreur){
-			session.setAttribute( ATT_SESSION_USER, prop );
-			request.setAttribute("apparts", apparts);
-			request.setAttribute("erreur", erreur);
-			request.setAttribute("parent", parent);
-	        this.getServletContext().getRequestDispatcher("/index.jsp").forward( request, response );
-		}
+       
     }
 }
